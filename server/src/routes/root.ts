0608fcc85be47fcb2dc4dd0fd9dbacbe960f8759
khant-min <kmin01405@gmail.com>
2023-12-07 23:00:31 +0630

    refactor: drop org server

--- server/src/routes/root.ts
+++
@@ -1,7 +0,0 @@
-import express, { Request, Response } from "express";
-import path from "path";
-const router = express.Router();
-
-export default router.get("/", (req: Request, res: Response): void => {
-  res.sendFile(path.join(__dirname, "..", "views", "index.html"));
-});
