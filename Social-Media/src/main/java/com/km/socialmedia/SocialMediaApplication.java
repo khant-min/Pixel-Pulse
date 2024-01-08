947870cd101a087d8ca6789c5fb6de3af6924b7b
Khant Min Thu <kmin01405@gmail.com>
2024-01-08 10:14:55 +0630

    fix: change server to spring boot

---
+++ Social-Media/src/main/java/com/km/socialmedia/SocialMediaApplication.java
@@ -0,0 +1,13 @@
+package com.km.socialmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaApplication.class, args);
+	}
+
+}
---
+++ Social-Media/src/test/java/com/km/socialmedia/SocialMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.km.socialmedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
